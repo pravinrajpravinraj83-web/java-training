fe5de6d9d2127519eef06a1c9c4e6f0ee6d26bab
ilayarajabala <ilayarajabala3gmail.com>
2026-07-21 18:15:54 +0530

    update

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -2,26 +2,19 @@ public class MoveZeroes {
 
     public static void moveZeroes(int[] nums) {
         int left = 0;
-
         for (int right = 0; right < nums.length; right++) {
-
             if (nums[right] != 0) {
-                // Swap nums[left] and nums[right]
                 int temp = nums[left];
                 nums[left] = nums[right];
                 nums[right] = temp;
-
                 left++;
             }
         }
     }
 
     public static void main(String[] args) {
-
         int[] nums = { 0, 1, 0, 3, 12 };
-
         moveZeroes(nums);
-
         System.out.print("Output: ");
         for (int num : nums) {
             System.out.print(num + " ");
--- RemoveLinkedListElements.java
+++ RemoveLinkedListElements.java
@@ -9,28 +9,21 @@ class ListNode {
 }
 
 public class RemoveLinkedListElements {
-
     public static ListNode removeElements(ListNode head, int val) {
-
         ListNode dummy = new ListNode(0);
         dummy.next = head;
-
         ListNode current = dummy;
-
         while (current.next != null) {
-
             if (current.next.data == val) {
                 current.next = current.next.next;
             } else {
                 current = current.next;
             }
         }
-
         return dummy.next;
     }
 
     public static void main(String[] args) {
-
         ListNode head = new ListNode(1);
         head.next = new ListNode(2);
         head.next.next = new ListNode(6);
@@ -38,14 +31,10 @@ public class RemoveLinkedListElements {
         head.next.next.next.next = new ListNode(4);
         head.next.next.next.next.next = new ListNode(5);
         head.next.next.next.next.next.next = new ListNode(6);
-
         head = removeElements(head, 6);
-
         while (head != null) {
             System.out.print(head.data + " ");
             head = head.next;
         }
     }
 }
-
-
